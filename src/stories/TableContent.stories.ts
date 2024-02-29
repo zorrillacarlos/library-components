98220fa07cdd97f87b8479dee2b397e1eb8b9fed
Carlos Zorrilla <zorrillacarlos4@gmail.com>
2024-02-29 13:56:50 -0400

    update TableContent stories

--- src/stories/TableContent.stories.ts
+++ src/stories/TableContent.stories.ts
@@ -1,49 +1,39 @@
 // Importa el componente
+import { ref } from "vue";
+import { defineComponent } from "vue";
 import TableContent from "../../components/TableUser/TableContent.vue";
 
-// Importa las interfaces
-interface TableColumn {
-  label: string;
-  field: string;
-}
-
-interface User {
-  name: string;
-  email: string;
-  age: number;
-}
-
-// Define la historia de Storybook
 export default {
-  title: "Components/TableContent",
+  title: "Componentes/TableContent",
   component: TableContent,
 };
 
-// Define los datos de ejemplo
-const tableColumns: TableColumn[] = [
-  { label: "Name", field: "name" },
-  { label: "Email", field: "email" },
-  { label: "Age", field: "age" },
-];
+const Template = (args: any) =>
+  defineComponent({
+    components: { TableContent },
+    setup() {
+      // Aquí se pueden definir datos reactivos o computados si es necesario
+      const userData = ref(args.userData);
+      return { args, userData };
+    },
+    template: `
+    <div>
+      <h2>Lista de Usuarios</h2>
+      <TableContent :columns="args.tableColumns" :items="userData" />
+    </div>
+  `,
+  });
 
-const userData: User[] = [
-  { name: "John", email: "john@example.com", age: 20 },
-  { name: "Alice", email: "alice@example.com", age: 25 },
-  { name: "Bob", email: "bob@example.com", age: 18 },
-];
-
-// Define el template de la historia
-const Template = (args: any) => ({
-  components: { TableContent },
-  setup() {
-    return { args };
-  },
-  template: '<TableContent v-bind="args" />',
-});
-
-// Define la historia Default
-export const TableComponent = Template.bind({});
-TableComponent.args = {
-  columns: tableColumns,
-  items: userData,
+export const Default = Template.bind({});
+Default.args = {
+  tableColumns: [
+    { label: "Name", field: "name" },
+    { label: "Email", field: "email" },
+    { label: "Age", field: "age" },
+  ],
+  userData: [
+    { name: "Juan", email: "juan@example.com", age: 30 },
+    { name: "Maria", email: "maria@example.com", age: 25 },
+    { name: "Pedro", email: "pedro@example.com", age: 35 },
+  ],
 };
